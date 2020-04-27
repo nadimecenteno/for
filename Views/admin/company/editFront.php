17a9cdbfc95a4e9da997c38e03e2f59b95d9abd6
Nadime <nadimecenteno@hotmail.com>
2020-04-26 21:22:49 -0300

    ultimas alterações do projeto front-end

--- Views/admin/company/editFront.php
+++ Views/admin/company/editFront.php
@@ -1,4 +1,7 @@
-    <!--Modal Cadastrar Empresa -->
+<html>
+    <!--<script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+Modal Cadastrar Empresa 
     <div class="modal fade" id="modal-editar-empresa" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -8,28 +11,43 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body">-->
                     
                     <form action="../admin/company/edit.php" method="post">
                     <!--<form action="/Treinamento Ecompjr/company/update < ?php $company->getId()?>" method="post">-->
                         <input type="hidden" name="method" value="update">
                         <input type="hidden" name="id" value="<?php echo $_SESSION['id'];?>">
-                        <div class="form-group">
+                        <div class="form-group p-4">
                             <label for="name">Nome da Empresa:</label>
                             <input type="text" class="form-control" name="name" id="name" value="<?php echo $_SESSION['name'];?>" placeholder="Informe o nome da empresa" required>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group p-4">
                             <label for="federation">Nome da Federação:</label>
                             <input type="text" class="form-control" name="federation" id="federation" value="<?php echo $_SESSION['federation'];?>" placeholder="Informe o nome da federação da empresa" required>
                         </div>
                         <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary">Salvar</button>
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-check"></i>
+                                Salvar
+                            </button>
+                            <!--<button type="button" class="btn btn-danger" data-dismiss="modal">
+                                <i class="fas fa-times"></i>
+                                Cancelar
+                            </button>-->
                         </div>
                         <!--<input name="name" value="< ?php echo $company->getName()?>" placeholder="name">-->
                         <!--<input name="federation" value="< ?php echo $company->getFederation()?>" placeholder="federation"> -->
                     </form>
-                </div>
+                <!--</div>
             </div>
         </div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(window).load(function() {
+                $('#modal-editar-empresa').modal('show');
+            });
+        });
+    </script>-->
+</html>
--- Views/admin/dashboard.php
+++ Views/admin/dashboard.php
@@ -9,7 +9,7 @@
         <title>Brasil Júnior - Página Inicial</title>
     </head>
     <body>
-        <div class="container">
+        <div class="container" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
             <div class="box">
 
                 <?php 
@@ -38,9 +38,9 @@
 
                     <!-- Collapse com Tabela de Listagem de Empresas -->
                     <div class="collapse p-2" id="collapse-listar-empresa">
-                        <div class="card card-body" id="card-listar-empresas">
+                        <div class="card card-body" id="card-listar-empresas" style=" box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
                             <div class="table-responsive">
-                                <table class="table table-striped table-active" id="cssTable" style="border-radius: 5px;">
+                                <table class="table table-striped table-active" id="cssTable" style="border-radius: 5px; margin-bottom: 0px;">
                                     <thead>
                                         <tr>
                                             <th scope="col">Nome</th>
@@ -76,14 +76,51 @@
                 </div>
             </div>
         </div>
+
+        
+    <div class="modal fade" id="modal-editar-empresa" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header" style="background-color: #007bff">
+                    <h5 class="modal-title" id="exampleModalLabel" style="color: white">Cadastrar Empresa</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    <form action="../admin/company/edit.php" method="post">
+                    <!--<form action="/Treinamento Ecompjr/company/update < ?php $company->getId()?>" method="post">-->
+                        <input type="hidden" name="method" value="update">
+                        <input type="hidden" name="id" value="<?php echo $_SESSION['id'];?>">
+                        <div class="form-group">
+                            <label for="name">Nome da Empresa:</label>
+                            <input type="text" class="form-control" name="name" id="name" value="<?php echo $_SESSION['name'];?>" placeholder="Informe o nome da empresa" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="federation">Nome da Federação:</label>
+                            <input type="text" class="form-control" name="federation" id="federation" value="<?php echo $_SESSION['federation'];?>" placeholder="Informe o nome da federação da empresa" required>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-check"></i>
+                                Salvar
+                            </button>
+                            <!--<button type="button" class="btn btn-danger" data-dismiss="modal">
+                                <i class="fas fa-times"></i>
+                                Cancelar
+                            </button>-->
+                        </div>
+                        <!--<input name="name" value="< ?php echo $company->getName()?>" placeholder="name">-->
+                        <!--<input name="federation" value="< ?php echo $company->getFederation()?>" placeholder="federation"> -->
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
         
     </body>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('#modal-editar-empresa').modal('show');
-        });
-    </script>
 </html>
